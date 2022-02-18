e77cd23988f7378d29589af2df6b2f76fa2f430f
simone <simonebenitozzi@gmail.com>
2022-02-18 22:38:00 +0100

    turned code into Jupyter notebook

--- mints.py
+++ mints.py
@@ -15,11 +15,20 @@ ds = nfts.dataset.FromSQLite(DATASET_PATH)
 
 # load current owners dataframe
 mints_df = ds.load_dataframe("mints")
+
 print(mints_df.head())
 
-##% Mints per Contract
+#%% Mints per Contract
 
+mint_stats_df = mints_df.groupby('nft_address', as_index=False).size().rename(columns={'size':'num_nfts'})
+mint_stats_df.hist('num_nfts', bins=200, log=True)
 
+mint_stats_df.quantile(q = np.arange(0.1, 1.1, 0.1))
+
+analysis_sample_df = mint_stats_df[mint_stats_df['num_nfts']>=100]
+analysis_sample_df.head()
+
+analysis_sample_df.count()
 
 #%% NFTs minted per address
 
