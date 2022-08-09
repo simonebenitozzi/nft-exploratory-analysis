dfbea6a58871290285bdde3746f3181d53f5aa0c
simone <simonebenitozzi@gmail.com>
2022-08-09 18:57:42 +0200

    utilities module

--- utilities/visualization_utils.py
+++ utilities/visualization_utils.py
@@ -1,11 +1,4 @@
-import numpy as np
 import seaborn as sns
-
-# average value for a DataFrame column, ignoring NaN's
-def nan_average(df, column):
-    not_nan_df = df[np.isnan(df[column]) == False]
-    avg = sum(not_nan_df[column]) / len(not_nan_df)
-    return avg
 
 # given a list of labels, assigns a color to each istance, based on its label
 def color_palette_mapping(labels):
