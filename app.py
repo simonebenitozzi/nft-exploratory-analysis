8e158c7668379138656b2b0099bda998d188e283
simone <simonebenitozzi@gmail.com>
2022-08-10 23:58:34 +0200

    graphical adjustments

--- app.py
+++ app.py
@@ -33,8 +33,11 @@ st.set_page_config(page_title="nfts Clutering", page_icon="img/NFT.png", layout=
 
 nfts_merged_df = pd.read_csv("data/nfts_merged.csv")
 pickle_dir = "data/pickle_vars/"
-ext = ".png"
 buffer = io.BytesIO()
+
+small_size = (6,4)
+medium_size = (10,7)
+big_size = (15,10)
 
 ### --- Preprocessing: Imputation of NaN Values --- ###
 
@@ -72,45 +75,50 @@ cumulative_sum_variance = np.cumsum(pca.explained_variance_ratio_)
 st.write(f"The next chart shows the cumulative Variance explained by each of the 6 Principal Components")
 st.write(f"The 4 Principal Components alone explain {round(sum(pca.explained_variance_ratio_[0:4])*10000)/100}% of the Variance: it makes sense then, that the Clustering algorithms will be executed on the 4 principal components")
 
-fig = plt.figure(figsize=(5,5))
+fig = plt.figure(figsize=small_size)
 plt.plot(range(1, len(cumulative_sum_variance)+1), cumulative_sum_variance)
 plt.xlabel("Number of Components")
 _ = plt.ylabel("Explained Variance (%)")
 _ = plt.title("Variance Accumulation")
 plt.savefig(buffer, format="png")
+
 st.image(buffer)
 
-## -- 2 Pricipal Components -- ##
-st.subheader("2 Principal Components")
+col1, col2 = st.columns(2)
 
-pca_2 = PCA(n_components=2)
-pca_2.fit_transform(x_pca)
-x_pca_2 = pca_2.transform(x_pca)
+with col1:
+    ## -- 2 Pricipal Components -- ##
+    st.subheader("2 Principal Components")
 
-fig = plt.figure(figsize=(10,7))
-sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50)
-_ = plt.title(f"2D Scatterplot: {round(cumulative_sum_variance[1]*10000)/100}% of variance captured")
-_ = plt.xlabel("First Principal Component")
-_ = plt.ylabel("Second Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+    pca_2 = PCA(n_components=2)
+    pca_2.fit_transform(x_pca)
+    x_pca_2 = pca_2.transform(x_pca)
 
-## -- 3 Pricipal Components -- ##
-st.subheader("3 Principal Components")
+    fig = plt.figure(figsize=small_size)
+    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50)
+    _ = plt.title(f"2D Scatterplot: {round(cumulative_sum_variance[1]*10000)/100}% of variance captured")
+    _ = plt.xlabel("First Principal Component")
+    _ = plt.ylabel("Second Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
 
-pca_3 = PCA(n_components=3)
-pca_3.fit_transform(x_pca)
-x_pca_3 = pca_3.transform(x_pca)
+with col2:
+    ## -- 3 Pricipal Components -- ##
+    st.subheader("3 Principal Components")
 
-fig = plt.figure(figsize=(15,10))
-ax = plt.axes(projection="3d")
-sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6)
-_ = plt.title(f"3D Scatterplot: {round(cumulative_sum_variance[2]*10000)/100}% of variance captured")
-_ = ax.set_xlabel("First Principal Component")
-_ = ax.set_ylabel("Second Principal Component")
-_ = ax.set_zlabel("Third Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+    pca_3 = PCA(n_components=3)
+    pca_3.fit_transform(x_pca)
+    x_pca_3 = pca_3.transform(x_pca)
+
+    fig = plt.figure(figsize=small_size)
+    ax = plt.axes(projection="3d")
+    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6)
+    _ = plt.title(f"3D Scatterplot: {round(cumulative_sum_variance[2]*10000)/100}% of variance captured")
+    _ = ax.set_xlabel("First Principal Component")
+    _ = ax.set_ylabel("Second Principal Component")
+    _ = ax.set_zlabel("Third Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
 
 ## -- 4 Pricipal Components -- ##
 st.subheader("4 Principal Components")
@@ -121,7 +129,7 @@ pca4_df = pd.DataFrame(principal_components)
 
 x_pca_4 = pca_4.transform(x_pca) # variable for scatterplot visualization
 
-fig = plt.figure(figsize=(15,10))
+fig = plt.figure(figsize=small_size)
 plt.plot(x_pca_4)
 _ = plt.title(f"Transformed Data by PCA: {round(cumulative_sum_variance[3]*10000)/100}% variance")
 _ = plt.xlabel("Observation")
@@ -129,9 +137,12 @@ _ = plt.ylabel("Transformed Data")
 plt.savefig(buffer, format="png")
 st.image(buffer)
 
+st.write("---")
+
 ### --- Clustering Tendency --- ###
 
 # TODO: VAT matrix needed: see if it can be executed in some more time
+
 
 ### --- K-Means --- ###
 st.header("K-Means Clustering")
@@ -148,22 +159,22 @@ except (OSError, IOError) as e:
         km = km.fit(x_pca_4)
         sum_squared_dist.append(km.inertia_)
     pickle.dump(sum_squared_dist, open(pickle_dir+"sum_squared_dist", "wb"))
-
 kn = KneeLocator(K, sum_squared_dist, curve='convex', direction='decreasing')
 
-fig = plt.figure(figsize=(10,7))
-_ = plt.plot(K,sum_squared_dist, 'bx-')
-_ = plt.title("Clustering Quality - Elbow Point")
-_ = plt.xlabel('number of clusters k')
-_ = plt.ylabel('Sum of squared distances')
-_ = plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed', label=f"Elbow point: {kn.knee}")
-_ = plt.legend()
-plt.savefig(buffer, format="png")
-st.image(buffer)
+col1, col2 = st.columns(2)
 
+with col1:
+    fig = plt.figure(figsize=small_size)
+    _ = plt.plot(K,sum_squared_dist, 'bx-')
+    _ = plt.title("Clustering Quality - Elbow Point")
+    _ = plt.xlabel('number of clusters k')
+    _ = plt.ylabel('Sum of squared distances')
+    _ = plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed', label=f"Elbow point: {kn.knee}")
+    _ = plt.legend()
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
 
 ## -- Silhouette Score -- ##
-
 K = range(2,15)
 try:
     silhouette_list = pickle.load(open(pickle_dir+"silhouette_list", "rb"))
@@ -176,14 +187,15 @@ except (OSError, IOError) as e:
         silhouette_list.append(silhouette_avg)
     pickle.dump(silhouette_list, open(pickle_dir+"silhouette_list", "wb"))
 
-fig = plt.figure(figsize=(10,7))
-_ = plt.plot(K,silhouette_list, 'bx-')
-_ = plt.title("Clustering Quality - Silhouette Score")
-_ = plt.xlabel('Number of clusters k')
-_ = plt.ylabel('Silhouette Score')
-_ = plt.xticks(K)
-plt.savefig(buffer, format="png")
-st.image(buffer)
+with col2:
+    fig = plt.figure(figsize=small_size)
+    _ = plt.plot(K,silhouette_list, 'bx-')
+    _ = plt.title("Clustering Quality - Silhouette Score")
+    _ = plt.xlabel('Number of clusters k')
+    _ = plt.ylabel('Silhouette Score')
+    _ = plt.xticks(K)
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
 
 # ## -- K Evaluation -- ##
 # K = [2, 3, 4, 5]
@@ -197,7 +209,7 @@ st.image(buffer)
 #         kms.append(KMeans(n_clusters=i, random_state=0))
 #     pickle.dump(kms, open(pickle_dir+"kms", "wb"))
 
-# fig, ax = plt.subplots(2, 2, figsize=(30,15))
+# fig, ax = plt.subplots(2, 2, figsize=big_size)
 # k = 2
 # for i in K:
 #     km = kms[k-2]
@@ -224,23 +236,28 @@ except (OSError, IOError) as e:
     km3 = KMeans(n_clusters=3, random_state=0).fit(x_pca_4)
     pickle.dump(km3, open(pickle_dir+"km3", "wb"))
 
-fig = plt.figure(figsize=(10,7))
-sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=km3.labels_, palette=sns.color_palette(None, 3))
-_ = plt.title(f"K-Means Clustering plotted on 2 Principal Components (K=3)")
-_ = plt.xlabel("First Principal Component")
-_ = plt.ylabel("Second Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+col1, col2 = st.columns(2)
 
-fig = plt.figure(figsize=(15,10))
-ax = plt.axes(projection="3d")
-sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(km3.labels_))
-_ = plt.title(f"K-Means Clustering plotted on 3 Principal Components (K=3)")
-_ = ax.set_xlabel("First Principal Component")
-_ = ax.set_ylabel("Second Principal Component")
-_ = ax.set_zlabel("Third Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+with col1:
+    fig = plt.figure(figsize=small_size)
+    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=km3.labels_, palette=sns.color_palette(None, 3))
+    _ = plt.title(f"K-Means Clustering plotted on 2 Principal Components (K=3)")
+    _ = plt.xlabel("First Principal Component")
+    _ = plt.ylabel("Second Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
+with col2:
+    fig = plt.figure(figsize=small_size)
+    ax = plt.axes(projection="3d")
+    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(km3.labels_))
+    _ = plt.title(f"K-Means Clustering plotted on 3 Principal Components (K=3)")
+    _ = ax.set_xlabel("First Principal Component")
+    _ = ax.set_ylabel("Second Principal Component")
+    _ = ax.set_zlabel("Third Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
+
+st.write("---")
 
 ### --- DBSCAN Clustering --- ###
 st.header("DBSCAN Clustering")
@@ -251,24 +268,30 @@ except (OSError, IOError) as e:
     dbscan = DBSCAN().fit(x_pca_4)
     pickle.dump(dbscan, open(pickle_dir+"dbscan", "wb"))
 
-n_clusters = len(np.unique(dbscan.labels_))
-fig = plt.figure(figsize=(10,7))
-sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=dbscan.labels_, palette=sns.color_palette(None, n_clusters))
-_ = plt.title(f"DBSCAN Clustering plotted on 2 Principal Components")
-_ = plt.xlabel("First Principal Component")
-_ = plt.ylabel("Second Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+col1, col2 = st.columns(2)
 
-fig = plt.figure(figsize=(15,10))
-ax = plt.axes(projection="3d")
-sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(dbscan.labels_+1))
-_ = plt.title(f"DBSCAN Clustering plotted on 3 Principal Components")
-_ = ax.set_xlabel("First Principal Component")
-_ = ax.set_ylabel("Second Principal Component")
-_ = ax.set_zlabel("Third Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+with col1:
+    n_clusters = len(np.unique(dbscan.labels_))
+    fig = plt.figure(figsize=small_size)
+    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=dbscan.labels_, palette=sns.color_palette(None, n_clusters))
+    _ = plt.title(f"DBSCAN Clustering plotted on 2 Principal Components")
+    _ = plt.xlabel("First Principal Component")
+    _ = plt.ylabel("Second Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
+
+with col2:
+    fig = plt.figure(figsize=small_size)
+    ax = plt.axes(projection="3d")
+    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(dbscan.labels_+1))
+    _ = plt.title(f"DBSCAN Clustering plotted on 3 Principal Components")
+    _ = ax.set_xlabel("First Principal Component")
+    _ = ax.set_ylabel("Second Principal Component")
+    _ = ax.set_zlabel("Third Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
+
+st.write("---")
 
 ### --- OPTICS Clustering --- ###
 st.header("OPTICS Clustering")
@@ -278,21 +301,24 @@ except (OSError, IOError) as e:
     optics = OPTICS().fit(x_pca_4)
     pickle.dump(optics, open(pickle_dir+"optics", "wb"))
 
-n_clusters = len(np.unique(optics.labels_))
-fig = plt.figure(figsize=(10,7))
-sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=optics.labels_, palette=sns.color_palette(None, n_clusters)).legend_.remove()
-_ = plt.title(f"OPTICS Clustering plotted on 2 Principal Components")
-_ = plt.xlabel("First Principal Component")
-_ = plt.ylabel("Second Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+col1, col2 = st.columns(2)
 
-fig = plt.figure(figsize=(15,10))
-ax = plt.axes(projection="3d")
-sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(optics.labels_))
-_ = plt.title(f"OPTICS Clustering plotted on 3 Principal Components")
-_ = ax.set_xlabel("First Principal Component")
-_ = ax.set_ylabel("Second Principal Component")
-_ = ax.set_zlabel("Third Principal Component")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+with col1:
+    n_clusters = len(np.unique(optics.labels_))
+    fig = plt.figure(figsize=small_size)
+    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=optics.labels_, palette=sns.color_palette(None, n_clusters)).legend_.remove()
+    _ = plt.title(f"OPTICS Clustering plotted on 2 Principal Components")
+    _ = plt.xlabel("First Principal Component")
+    _ = plt.ylabel("Second Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
+with col2:
+    fig = plt.figure(figsize=small_size)
+    ax = plt.axes(projection="3d")
+    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(optics.labels_))
+    _ = plt.title(f"OPTICS Clustering plotted on 3 Principal Components")
+    _ = ax.set_xlabel("First Principal Component")
+    _ = ax.set_ylabel("Second Principal Component")
+    _ = ax.set_zlabel("Third Principal Component")
+    plt.savefig(buffer, format="png")
+    st.image(buffer)
