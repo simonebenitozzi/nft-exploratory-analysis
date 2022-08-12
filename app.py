d706a5bd866c4a90da108c9f93ed0ec6c00c9b0f
simone <simonebenitozzi@gmail.com>
2022-08-12 13:23:51 +0200

    converted all figures to plotly express

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import title
 import streamlit as st
 
 from utils.visualization import color_palette_mapping
@@ -8,8 +9,11 @@ import pickle
 import io 
 
 import matplotlib.pyplot as plt
+from matplotlib import style
 from mpl_toolkits import mplot3d
+
 import seaborn as sns
+import plotly.express as px
 
 import numpy as np
 # import nfts.dataset
@@ -30,17 +34,13 @@ from kneed import KneeLocator
 ### --- Streamlit Configuration --- ###
 
 st.set_page_config(page_title="nfts Clutering", page_icon="img/NFT.png", layout="wide")
+style.use("seaborn")
 
 nfts_merged_df = pd.read_csv("data/nfts_merged.csv")
 pickle_dir = "data/pickle_vars/"
-buffer = io.BytesIO()
-
-small_size = (6,4)
-medium_size = (10,7)
-big_size = (15,10)
 
 ### --- Preprocessing: Imputation of NaN Values --- ###
-
+# TODO: save df already filled
 # MCM (Most Common Value) Imputation for mints_values, transfers_values, market_values
 nfts_merged_df.mints_avg_transaction_value.fillna(nan_average(nfts_merged_df, "mints_avg_transaction_value"), inplace=True)
 nfts_merged_df.transfers_avg_transaction_value.fillna(nan_average(nfts_merged_df, "transfers_avg_transaction_value"), inplace=True)
@@ -51,6 +51,12 @@ nfts_merged_df.mints_timestamp_range.fillna(0, inplace=True)
 nfts_merged_df.transfers_count.fillna(0, inplace=True)
 nfts_merged_df.num_owners.fillna(0, inplace=True)
 
+custom_cols = [nfts_merged_df['name'], nfts_merged_df['symbol'], 
+                round(nfts_merged_df['avg_market_value']*100)/100, nfts_merged_df['num_owners']]
+custom_template = """<b>%{customdata[0]} (%{customdata[1]})</b><br><br>
+Avg Value: %{customdata[2]} eth<br>
+Owners: %{customdata[3]}
+<extra></extra>"""
 
 ### --- Principal Component Analysis --- ###
 st.header("Principal Component Analysis")
@@ -71,18 +77,20 @@ pca = PCA()
 
 principal_components = pca.fit_transform(x_pca)
 cumulative_sum_variance = np.cumsum(pca.explained_variance_ratio_)
+cumulative_sum_variance = np.around(cumulative_sum_variance*100, 2)
 
 st.write(f"The next chart shows the cumulative Variance explained by each of the 6 Principal Components")
-st.write(f"The 4 Principal Components alone explain {round(sum(pca.explained_variance_ratio_[0:4])*10000)/100}% of the Variance: it makes sense then, that the Clustering algorithms will be executed on the 4 principal components")
+st.write(f"""The 4 Principal Components alone explain {cumulative_sum_variance[4]}% 
+of the Variance: it makes sense then, that the Clustering algorithms will be executed on the 4 principal components""")
 
-fig = plt.figure(figsize=small_size)
-plt.plot(range(1, len(cumulative_sum_variance)+1), cumulative_sum_variance)
-plt.xlabel("Number of Components")
-_ = plt.ylabel("Explained Variance (%)")
-_ = plt.title("Variance Accumulation")
-plt.savefig(buffer, format="png")
-
-st.image(buffer)
+fig = px.line(x=range(1, len(cumulative_sum_variance)+1), y=cumulative_sum_variance, 
+                labels={"x":"Number of Components", "y":"Explained Variance (%)"}, text=cumulative_sum_variance,
+                title="<b>Variance Accumulation", custom_data=[range(1,7), cumulative_sum_variance])
+fig.update_layout(xaxis={"range":[0.5, len(cumulative_sum_variance)+0.5]},
+                    yaxis={"range":[0,110]}, showlegend=False, hovermode="x")
+fig.update_traces(textposition="top center", hovertemplate="Principal Components: %{customdata[0]}<br>Variance explained: %{customdata[1]}<extra></extra>")
+fig.add_bar(x=list(range(1, len(cumulative_sum_variance)+1)), y=cumulative_sum_variance)
+st.write(fig)
 
 col1, col2 = st.columns(2)
 
@@ -93,14 +101,13 @@ with col1:
     pca_2 = PCA(n_components=2)
     pca_2.fit_transform(x_pca)
     x_pca_2 = pca_2.transform(x_pca)
+    pca2_df = pd.DataFrame(x_pca_2, columns=["pc1", "pc2"])
 
-    fig = plt.figure(figsize=small_size)
-    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50)
-    _ = plt.title(f"2D Scatterplot: {round(cumulative_sum_variance[1]*10000)/100}% of variance captured")
-    _ = plt.xlabel("First Principal Component")
-    _ = plt.ylabel("Second Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter(x=x_pca_2[:,0], y=x_pca_2[:,1], labels={"x": "First Principal Component", "y":"Second Principal Component"},
+                        title=f"<b>2D Scatterplot: {cumulative_sum_variance[1]}% of variance captured",
+                        custom_data=custom_cols)
+    fig.update_traces(hovertemplate=custom_template)
+    st.write(fig)
 
 with col2:
     ## -- 3 Pricipal Components -- ##
@@ -110,32 +117,39 @@ with col2:
     pca_3.fit_transform(x_pca)
     x_pca_3 = pca_3.transform(x_pca)
 
-    fig = plt.figure(figsize=small_size)
-    ax = plt.axes(projection="3d")
-    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6)
-    _ = plt.title(f"3D Scatterplot: {round(cumulative_sum_variance[2]*10000)/100}% of variance captured")
-    _ = ax.set_xlabel("First Principal Component")
-    _ = ax.set_ylabel("Second Principal Component")
-    _ = ax.set_zlabel("Third Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter_3d(x=x_pca_3[:,0], y=x_pca_3[:,1], z=x_pca_3[:,2], 
+                        labels={"x": "First Principal Component", "y":"Second Principal Component", "z":"Third Principal Component"},
+                        title=f"<b>3D Scatterplot: {cumulative_sum_variance[2]}% of variance captured",
+                        custom_data=custom_cols)
+    fig.update_traces(marker_size = 3, hovertemplate=custom_template)
+    st.write(fig)
 
 ## -- 4 Pricipal Components -- ##
-st.subheader("4 Principal Components")
+# TODO: change n dinamically ?
+st.subheader("Observation vs Transformed Data")
 
-pca_4 = PCA(n_components=4)
-principal_components = pca_4.fit_transform(x_pca)
+N = st.slider("Number of Principal Components:", 1, 6, 4)
+
+pca_n = PCA(n_components=N)
+principal_components = pca_n.fit_transform(x_pca)
 pca4_df = pd.DataFrame(principal_components)
 
-x_pca_4 = pca_4.transform(x_pca) # variable for scatterplot visualization
+x_pca_n = pca_n.transform(x_pca) # variable for scatterplot visualization
 
-fig = plt.figure(figsize=small_size)
-plt.plot(x_pca_4)
-_ = plt.title(f"Transformed Data by PCA: {round(cumulative_sum_variance[3]*10000)/100}% variance")
-_ = plt.xlabel("Observation")
-_ = plt.ylabel("Transformed Data")
-plt.savefig(buffer, format="png")
-st.image(buffer)
+fig = px.line(x_pca_n, labels={"index":"Observation", "value":"Transformed Data"},
+                title=f"<b>Transformed Data by PCA: {cumulative_sum_variance[N-1]}% variance</b>")
+# newnames = {'0':'PC1', '1': 'PC2', '2':'PC3', '3':'PC4'}
+new_names = {}
+for i in range(N):
+    new_names[str(i)] = "PC"+str(i+1)
+
+fig.for_each_trace(lambda t: t.update(name = new_names[t.name],
+                                      legendgroup = new_names[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, new_names[t.name])
+                                     )
+                  )
+fig.update_layout(legend_title="Principal Components")
+st.write(fig)
 
 st.write("---")
 
@@ -147,6 +161,8 @@ st.write("---")
 ### --- K-Means --- ###
 st.header("K-Means Clustering")
 
+st.subheader("Choice of K")
+
 ## -- Elbow Method -- ##
 
 K = range(1,50)
@@ -156,7 +172,7 @@ except (OSError, IOError) as e:
     sum_squared_dist = []
     for k in K:
         km = KMeans(n_clusters=k, random_state=0)
-        km = km.fit(x_pca_4)
+        km = km.fit(x_pca_n)
         sum_squared_dist.append(km.inertia_)
     pickle.dump(sum_squared_dist, open(pickle_dir+"sum_squared_dist", "wb"))
 kn = KneeLocator(K, sum_squared_dist, curve='convex', direction='decreasing')
@@ -164,15 +180,11 @@ kn = KneeLocator(K, sum_squared_dist, curve='convex', direction='decreasing')
 col1, col2 = st.columns(2)
 
 with col1:
-    fig = plt.figure(figsize=small_size)
-    _ = plt.plot(K,sum_squared_dist, 'bx-')
-    _ = plt.title("Clustering Quality - Elbow Point")
-    _ = plt.xlabel('number of clusters k')
-    _ = plt.ylabel('Sum of squared distances')
-    _ = plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed', label=f"Elbow point: {kn.knee}")
-    _ = plt.legend()
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.line(x=K, y=sum_squared_dist, labels={"x":"number of clusters - k", "y":"Sum of squared distances"},
+                title="<b>Clustering Quality - Elbow Point")
+    fig.add_vline(x=kn.knee, line_width=1.5, line_dash="dash", line_color="green", annotation_text=f"Elbow point: {kn.knee}")
+    fig.update_layout(xaxis={"range":[min(K)-1,max(K)+1]})
+    st.write(fig)
 
 ## -- Silhouette Score -- ##
 K = range(2,15)
@@ -182,20 +194,16 @@ except (OSError, IOError) as e:
     silhouette_list = []
     for k in K:
         km = KMeans(n_clusters=k, random_state=0)
-        km_labels = km.fit_predict(x_pca_4)
-        silhouette_avg = silhouette_score(x_pca_4, km_labels)
+        km_labels = km.fit_predict(x_pca_n)
+        silhouette_avg = silhouette_score(x_pca_n, km_labels)
         silhouette_list.append(silhouette_avg)
     pickle.dump(silhouette_list, open(pickle_dir+"silhouette_list", "wb"))
 
 with col2:
-    fig = plt.figure(figsize=small_size)
-    _ = plt.plot(K,silhouette_list, 'bx-')
-    _ = plt.title("Clustering Quality - Silhouette Score")
-    _ = plt.xlabel('Number of clusters k')
-    _ = plt.ylabel('Silhouette Score')
-    _ = plt.xticks(K)
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.line(x=K, y=silhouette_list, labels={"x":"Number of clusters - k", "y":"Silhouette Score"},
+                    title="<b>Clustering Quality - Silhouette Score", markers=True)
+    fig.update_layout(xaxis={"dtick":2, "range":[min(K)-1,max(K)+1]})
+    st.write(fig)
 
 # ## -- K Evaluation -- ##
 # K = [2, 3, 4, 5]
@@ -233,29 +241,27 @@ st.subheader("K-Means with K=3")
 try:
     km3 = pickle.load(open(pickle_dir+"km3", "rb"))
 except (OSError, IOError) as e:
-    km3 = KMeans(n_clusters=3, random_state=0).fit(x_pca_4)
+    km3 = KMeans(n_clusters=3, random_state=0).fit(x_pca_n)
     pickle.dump(km3, open(pickle_dir+"km3", "wb"))
+colors = color_palette_mapping(km3.labels_)
 
 col1, col2 = st.columns(2)
-
 with col1:
-    fig = plt.figure(figsize=small_size)
-    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=km3.labels_, palette=sns.color_palette(None, 3))
-    _ = plt.title(f"K-Means Clustering plotted on 2 Principal Components (K=3)")
-    _ = plt.xlabel("First Principal Component")
-    _ = plt.ylabel("Second Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter(x=x_pca_2[:,0], y=x_pca_2[:,1], labels={"x": "First Principal Component", "y":"Second Principal Component"},
+                        title=f"<b>K-Means Clustering plotted on 2 Principal Components (K=3)",
+                        custom_data=custom_cols, color=colors)
+    fig.update_traces(hovertemplate=custom_template)
+    fig.update_layout(showlegend=False)
+    st.write(fig)
+
 with col2:
-    fig = plt.figure(figsize=small_size)
-    ax = plt.axes(projection="3d")
-    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(km3.labels_))
-    _ = plt.title(f"K-Means Clustering plotted on 3 Principal Components (K=3)")
-    _ = ax.set_xlabel("First Principal Component")
-    _ = ax.set_ylabel("Second Principal Component")
-    _ = ax.set_zlabel("Third Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter_3d(x=x_pca_3[:,0], y=x_pca_3[:,1], z=x_pca_3[:,2], 
+                        labels={"x": "First Principal Component", "y":"Second Principal Component", "z":"Third Principal Component"},
+                        title=f"<b>K-Means Clustering plotted on 3 Principal Components (K=3)",
+                        custom_data=custom_cols, color=colors)
+    fig.update_traces(marker_size = 3, hovertemplate=custom_template)
+    fig.update_layout(showlegend=False)
+    st.write(fig)
 
 st.write("---")
 
@@ -265,31 +271,27 @@ st.header("DBSCAN Clustering")
 try:
     dbscan = pickle.load(open(pickle_dir+"dbscan", "rb"))
 except (OSError, IOError) as e:
-    dbscan = DBSCAN().fit(x_pca_4)
+    dbscan = DBSCAN().fit(x_pca_n)
     pickle.dump(dbscan, open(pickle_dir+"dbscan", "wb"))
+colors = color_palette_mapping(dbscan.labels_)
 
 col1, col2 = st.columns(2)
-
 with col1:
-    n_clusters = len(np.unique(dbscan.labels_))
-    fig = plt.figure(figsize=small_size)
-    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=dbscan.labels_, palette=sns.color_palette(None, n_clusters))
-    _ = plt.title(f"DBSCAN Clustering plotted on 2 Principal Components")
-    _ = plt.xlabel("First Principal Component")
-    _ = plt.ylabel("Second Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter(x=x_pca_2[:,0], y=x_pca_2[:,1], labels={"x": "First Principal Component", "y":"Second Principal Component"},
+                        title=f"<b>DBSCAN Clustering plotted on 2 Principal Components",
+                        custom_data=custom_cols, color=colors)
+    fig.update_traces(hovertemplate=custom_template)
+    fig.update_layout(showlegend=False)
+    st.write(fig)
 
 with col2:
-    fig = plt.figure(figsize=small_size)
-    ax = plt.axes(projection="3d")
-    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(dbscan.labels_+1))
-    _ = plt.title(f"DBSCAN Clustering plotted on 3 Principal Components")
-    _ = ax.set_xlabel("First Principal Component")
-    _ = ax.set_ylabel("Second Principal Component")
-    _ = ax.set_zlabel("Third Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter_3d(x=x_pca_3[:,0], y=x_pca_3[:,1], z=x_pca_3[:,2], 
+                        labels={"x": "First Principal Component", "y":"Second Principal Component", "z":"Third Principal Component"},
+                        title=f"<b>DBSCAN Clustering plotted on 3 Principal Components",
+                        custom_data=custom_cols, color=colors)
+    fig.update_traces(marker_size = 3, hovertemplate=custom_template)
+    fig.update_layout(showlegend=False)
+    st.write(fig)
 
 st.write("---")
 
@@ -298,27 +300,23 @@ st.header("OPTICS Clustering")
 try:
     optics = pickle.load(open(pickle_dir+"optics", "rb"))
 except (OSError, IOError) as e:
-    optics = OPTICS().fit(x_pca_4)
+    optics = OPTICS().fit(x_pca_n)
     pickle.dump(optics, open(pickle_dir+"optics", "wb"))
+colors = color_palette_mapping(optics.labels_)
 
 col1, col2 = st.columns(2)
-
 with col1:
-    n_clusters = len(np.unique(optics.labels_))
-    fig = plt.figure(figsize=small_size)
-    sns.scatterplot(x=x_pca_2[:,0], y=x_pca_2[:,1], s=50, hue=optics.labels_, palette=sns.color_palette(None, n_clusters)).legend_.remove()
-    _ = plt.title(f"OPTICS Clustering plotted on 2 Principal Components")
-    _ = plt.xlabel("First Principal Component")
-    _ = plt.ylabel("Second Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter(x=x_pca_2[:,0], y=x_pca_2[:,1], labels={"x": "First Principal Component", "y":"Second Principal Component"},
+                        title=f"<b>OPTICS Clustering plotted on 2 Principal Components",
+                        custom_data=custom_cols, color=colors)
+    fig.update_traces(hovertemplate=custom_template)
+    fig.update_layout(showlegend=False)
+    st.write(fig)
 with col2:
-    fig = plt.figure(figsize=small_size)
-    ax = plt.axes(projection="3d")
-    sctt = ax.scatter3D(x_pca_3[:,0], x_pca_3[:,1], x_pca_3[:,2], s=50, alpha=0.6, c=color_palette_mapping(optics.labels_))
-    _ = plt.title(f"OPTICS Clustering plotted on 3 Principal Components")
-    _ = ax.set_xlabel("First Principal Component")
-    _ = ax.set_ylabel("Second Principal Component")
-    _ = ax.set_zlabel("Third Principal Component")
-    plt.savefig(buffer, format="png")
-    st.image(buffer)
+    fig = px.scatter_3d(x=x_pca_3[:,0], y=x_pca_3[:,1], z=x_pca_3[:,2], 
+                        labels={"x": "First Principal Component", "y":"Second Principal Component", "z":"Third Principal Component"},
+                        title=f"<b>OPTICS Clustering plotted on 3 Principal Components",
+                        custom_data=custom_cols, color=colors)
+    fig.update_traces(marker_size = 3, hovertemplate=custom_template)
+    fig.update_layout(showlegend=False)
+    st.write(fig)
